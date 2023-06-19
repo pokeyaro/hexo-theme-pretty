12e41ff7ea161b484e8cea74f153559d7629bcd5
pokeya <pokeya.mystic@gmail.com>
2023-06-19 20:48:00 +0800

    chore: 将 script 转换为更为通用 Makefile 文件

--- start.sh
+++ start.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-# 判断本地是或有 Node 环境
-if ! command -v node &> /dev/null; then
-  echo "Error: Node.js is not installed. Please install Node.js before running this script."
-  exit 1
-fi
+# 环境检查
+make check
 
-# 判断本地是否有 Yarn 工具
-if ! command -v yarn &> /dev/null; then
-  echo "Error: yarn is not installed. Please install yarn before running this script."
-  exit 1
-fi
+# 检查并安装所需的依赖包
+make install
 
-# 判断本地是否安装 Hexo 库
-if ! command -v hexo &> /dev/null; then
-  echo "Error: Hexo is not installed. Please install Hexo before running this script."
-  exit 1
-fi
-
-# 下载依赖包
-yarn
-
-if [ ! -d "./node_modules" ]; then
-  exit 1
-fi
-
-# 渲染静态网站
-hexo generate
+# 渲染生成静态网站
+make generate
 
 # 启动 Hexo 本地服务器
-hexo server
+make server
 
 #EOF
