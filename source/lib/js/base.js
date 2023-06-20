9d60b4075c76ee1a9922b5d188bb695ff693bd05
Mystic <chenbo.mystic@bytedance.com>
2023-06-20 23:45:41 +0800

    feat: 头部导航栏居中显示，隐藏"搜索"字样

---
+++ source/lib/js/base.js
@@ -0,0 +1,4 @@
+var searchButton = document.querySelector("#search-button");
+var linkElement = searchButton.querySelector("a");
+var spanElement = linkElement.querySelector("span");
+spanElement.style.display = "none";
